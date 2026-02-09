4adfb9c4570177ab89a18f7553928b38b3798920
Rene Illetschko <reneilletschko@gmx.net>
2026-02-09 21:06:11 +0100

    Update firmware version to 1.9 and adjust LED delay timings in main.cpp

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -16,8 +16,8 @@ constexpr unsigned long RESET_HOLD_MS = 5000; // Zeit bis Reset ausgelöst wird 
 constexpr unsigned long BOOT_IGNORE_MS = 3000; // GPIO0 beim Boot ignorieren
 
 // ===================== Firmware =====================
-const char* currentFirmwareVersion = "1.8";
-const char* firmwareUrl = "https://github.com/reneilletschko/race-frame/releases/download/updates/firmware.bin"; //https://github.com/ittipu/esp32_firmware/releases/download/esp32_firmware/firmware.ino.bin
+const char* currentFirmwareVersion = "1.9";
+const char* firmwareUrl = "https://github.com/reneilletschko/race-frame/releases/download/main/firmware.bin"; //https://github.com/ittipu/esp32_firmware/releases/download/esp32_firmware/firmware.ino.bin
 const char* versionUrl = "https://raw.githubusercontent.com/reneilletschko/race-frame/refs/heads/main/Firmware/version.txt"; //https://raw.githubusercontent.com/ittipu/esp32_firmware/refs/heads/main/version.txt
 const unsigned long updateCheckInterval = 5 * 60 * 1000;  // 5 minutes in milliseconds
 unsigned long lastUpdateCheck = 0;
@@ -335,10 +335,10 @@ void loop() {
     lastUpdateCheck = currentMillis;
     checkForFirmwareUpdate();}
   setLed(255, 0, 0);
-  delay(500);
+  delay(200);
   setLed(0, 255, 0);
-  delay(500);
+  delay(200);
   setLed(0, 0, 255);
-  delay(500);
+  delay(200);
 
 }
